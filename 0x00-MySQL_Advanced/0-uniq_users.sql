90fabc84c1e2ee1788f5a6f2e501d64214dbce66
root <doreenkinya153@gmail.com>
2023-12-14 21:19:25 +0000

    script that creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,10 @@
--- script that creates a table users
+-- Creates a table with unique users.
+
 DROP TABLE IF EXISTS users;
 
-CREATE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
+CREATE TABLE
+    users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+    );
