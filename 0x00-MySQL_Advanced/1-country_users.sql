9465c46db8389bcb1e3092a22ac360a5f134392d
root <doreenkinya153@gmail.com>
2023-12-14 21:24:08 +0000

    script that creates a table users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,11 @@
--- script that creates a table users
+-- Creates a table with unique users.
 
-CREATE TABLE IF NOT EXIST users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+DROP TABLE IF EXISTS users;
 
-);
+CREATE TABLE
+    users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+    );
