53193b7713311b24d2e30375853c460b6b12a3a2
root <doreenkinya153@gmail.com>
2023-12-22 20:51:08 +0000

    doccumentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -66,47 +66,47 @@ def replay(method: Callable) -> None:
 
 
 class Cache:
-    '''
+    """
         Cache class.
-    '''
+    """
     def __init__(self):
-        '''
+        """
             Initialize the cache.
-        '''
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''
+        """
             Store data in the cache.
-        '''
+        """
         randomKey = str(uuid4())
         self._redis.set(randomKey, data)
         return randomKey
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        '''
+        """
             Get data from the cache.
-        '''
+        """
         value = self._redis.get(key)
         if fn:
             value = fn(value)
         return value
 
     def get_str(self, key: str) -> str:
-        '''
+        """
             Get a string from the cache.
-        '''
+        """
         value = self._redis.get(key)
         return value.decode('utf-8')
 
     def get_int(self, key: str) -> int:
-        '''
+        """
             Get an int from the cache.
-        '''
+        """
         value = self._redis.get(key)
         try:
             value = int(value.decode('utf-8'))
