4bb3f07fa0af713b9e37885212b54f64a02be643
root <doreenkinya153@gmail.com>
2024-01-09 13:32:26 +0000

    Creates a table with unique users.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -7,5 +7,4 @@ CREATE TABLE
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255)
-	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
     );
